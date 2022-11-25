030b4ac3c90dc4254f84dbb28e3c2a0f2abc62b7
dhunganabibek <Dhunganabibek2016@gmail.com>
2022-11-25 15:08:26 -0600

    Interup, sleep and join done

--- README.md
+++ README.md
@@ -1,1 +1,45 @@
 # Multi Threading class by JavaBrain
+
+
+## Thread creation
+```
+a. extending Thread class
+b. using Runnable interface (Anonymous inner class and lambda expression)
+
+```
+
+
+## Setting thread to be daemon so it do not main thread can quit without waiting for it
+```
+t.setDaemon(true);
+```
+
+
+## Thread Lifecycle
+```
+a. new
+b. Runnable
+c. Blocked 
+d. Waiting
+e. Timed Waiting
+f. Terminated
+```
+
+## Waiting for the Thread
+```
+Thread.sleep() -- thread sleep for certain amount of time.
+myThread.join() -- the caller thread excute after myThread is completed.
+
+```
+
+## stopping a Thread
+```
+a. Do not use myThread.stop()   -- Deprecreted
+b. can interrupt a thread.  -- t.interrupt(). 
+ -- allows you to do clean up before thread dies.
+
+```
+
+
+
+
--- src/main/java/com/bibekdhungana/App.java
+++ src/main/java/com/bibekdhungana/App.java
@@ -1,10 +1,19 @@
 package com.bibekdhungana;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class App {
 
-  public static void main(String[] args) {
+  public static void threadStatusInfo(List<Thread> threads)
+    throws InterruptedException {
+    System.out.println("-------------Thread Status----------------");
+    threads.forEach(thread -> System.out.println(thread.getState()));
+  }
+
+  public static void main(String[] args) throws Exception {
+    final List<Thread> threads = new ArrayList<Thread>();
     while (true) {
       Scanner sc = new Scanner(System.in);
       System.out.println("I can tell you the nth Prime Number. Enter n ");
@@ -20,7 +29,21 @@ public class App {
         }
       };
       Thread t = new Thread(r);
+      t.setDaemon(true);
+      threads.add(t);
       t.start();
     }
+    Thread reportThread = new Thread(() -> {
+      while (true) {
+        try {
+          threadStatusInfo(threads);
+          Thread.sleep(1000);
+        } catch (Exception e) {
+          System.out.println(e);
+        }
+      }
+    });
+
+    reportThread.start();
   }
 }
