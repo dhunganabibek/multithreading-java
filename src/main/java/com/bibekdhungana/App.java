0cd6eeec7ddf30eb6c33a13bdb7851b539962731
dhunganabibek <Dhunganabibek2016@gmail.com>
2022-11-25 12:39:09 -0600

    prime number calculation and mvn jar plugin added

--- src/main/java/com/bibekdhungana/App.java
+++ src/main/java/com/bibekdhungana/App.java
@@ -5,21 +5,22 @@ import java.util.Scanner;
 public class App {
 
   public static void main(String[] args) {
-    System.out.println("Hello World!");
-
-    int number = 0;
-
     while (true) {
       Scanner sc = new Scanner(System.in);
       System.out.println("I can tell you the nth Prime Number. Enter n ");
-      int n = sc.nextInt();
+      final int n = sc.nextInt();
 
-      if (n == 0) {
-        break;
-      }
+      if (n == 0) break;
 
-      number = PrimeNumberUtil.calculatePrime(n);
-      System.out.println("The value of " + n + "th prime: " + number);
+      Runnable r = new Runnable() {
+        @Override
+        public void run() {
+          int number = PrimeNumberUtil.calculatePrime(n);
+          System.out.println("The value of " + n + "th prime: " + number);
+        }
+      };
+      Thread t = new Thread(r);
+      t.start();
     }
   }
 }
